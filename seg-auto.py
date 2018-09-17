64ba0c37887458231b6602a38a789ac02fc4adbe
ben <benhalstd@gmail.com>
2018-09-17 23:19:58 +1200

    clean up code

--- seg-auto.py
+++ seg-auto.py
@@ -6,24 +6,23 @@ from skimage.feature import greycomatrix, greycoprops
 from skimage import data
 from skimage.color import rgb2gray
 import argparse
-# import mahotas as mt
 import json
 import os
 
+# Extract and set command line arguments for parameters
 parser = argparse.ArgumentParser()
 parser.add_argument("--blocksize", type=int, help="enter some quality limit",
                     nargs='?', default=10)
 parser.add_argument("--multi", type=float, help="enter some quality limit",
-                    nargs='?', default=2)
-parser.add_argument("--texthresh", type=int, help="enter some quality limit",
-                    nargs='?', default=100)
+                    nargs='?', default=1.9)
+parser.add_argument("--texthresh", type=float, help="enter some quality limit",
+                    nargs='?', default=0.1)
 parser.add_argument("--colthresh", type=float, help="enter some quality limit",
-                    nargs='?', default=0.7)
+                    nargs='?', default=0.5)
 parser.add_argument("--file", help="enter some quality limit",
                     nargs='?', default='miro')
 parser.add_argument("--detail", help="enter some quality limit", action='store_true', default=False)
 args = parser.parse_args()
-print(args)
 BLOCK_SIZE = args.blocksize
 COLTHRESH = args.colthresh
 TEXTHRESH = args.texthresh
@@ -31,10 +30,12 @@ FILE = args.file
 DETAIL = args.detail
 MULTI = args.multi
 
+# Creates output directory
 o_dir = "output//{0}-{1}-{2}-{3}-{4}//".format(FILE, BLOCK_SIZE, COLTHRESH, TEXTHRESH, MULTI)
 if not os.path.exists(o_dir):
     os.makedirs(o_dir)
 
+# Load in image, convert to HSV
 img_orig = cv2.imread('input//{0}.jpg'.format(FILE),1)
 img = cv2.imread('input//{0}.jpg'.format(FILE),1)
 img_orig = cv2.cvtColor(img_orig, cv2.COLOR_BGR2HSV)
@@ -66,16 +67,15 @@ bottom = math.ceil(extra_rows / 2)
 left = math.floor(extra_cols / 2)
 right = math.ceil(extra_cols / 2)
 
-print(img.shape)
 img_bigger = cv2.copyMakeBorder(img_orig, top, bottom, left, right, cv2.BORDER_REPLICATE)
 
-# img_grey = cv2.cvtColor(img_bigger, cv2.COLOR_RGB2GRAY)
+# Get the grey values (only works with HSV)
 img_grey =img_bigger[..., 2]
-print(img_bigger.shape)
 
 windows = []
 bins = []
 
+# If asked, prints out image with windows overlayed
 if DETAIL:
     init_rects = img_bigger.copy()
     for row in range(0, blocks_y):
@@ -89,82 +89,74 @@ if DETAIL:
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+# Creates windows, gets their texture and color histograms
 for row in range(0, blocks_y):
-# for row in range(0, 1):
     for col in range(0, blocks_x):
-        print(img_grey.shape)
         print("Row {0}/{2} Col {1}/{3}".format(row, col, blocks_y, blocks_x))
         block_id = row * blocks_x + col
         t = row * BLOCK_SIZE
         l = col * BLOCK_SIZE
         b = (row+1) * BLOCK_SIZE
         r = (col+1) * BLOCK_SIZE
-        #print("({0},{1}), ({2},{3})".format(l,t, r, b))
+
         window = img_bigger[t:b, l:r]
         window_grey = img_grey[t:b, l:r]
         window_grey = window_grey.astype(int)
-        #print(window_grey.shape)
+
+        # Texture metrics are Harlick features
         glcm = greycomatrix(window_grey, [5], [0], 256, symmetric=True, normed=True)
         dis = greycoprops(glcm, 'dissimilarity')[0, 0]
         con = greycoprops(glcm, 'contrast')[0, 0]
         cor = greycoprops(glcm, 'correlation')[0, 0]
         texture = np.array([dis, con, cor])
-        # textures = mt.features.haralick(window_grey)
-        
-        # # take the mean of it and return it
-        # texture = textures.mean(axis=0)
-        #print(texture)
-        # hist = cv2.calcHist([window], [0, 1, 2], None, [8, 8, 8],[0, 256, 0, 256, 0, 256])
+
+        # Color metric is the image histogram (In HSV)
         hist = cv2.calcHist([window], [0, 1, 2], None, [18, 25, 25],[0, 180, 0, 256, 0, 256])
         hist = cv2.normalize(hist, None).flatten()
+
+        windows.append([block_id, texture, hist, window, None, [l, t, r, b]])
+
+# Normalizes texture features
+dis_vals = list(map(lambda w: w[1][0], windows))
+dis_max = max(dis_vals)
+dis_min = min(dis_vals)
+con_vals = list(map(lambda w: w[1][1], windows))
+con_max = max(con_vals)
+con_min = min(con_vals)
+cor_vals = list(map(lambda w: w[1][2], windows))
+cor_max = max(cor_vals)
+cor_min = min(cor_vals)
+for a in windows:
+    a[1][0] = ((a[1][0] - dis_min) / (dis_max - dis_min))
+    a[1][1] = ((a[1][1] - con_min) / (con_max - con_min))
+    a[1][2] = ((a[1][2] - cor_min) / (cor_max - cor_min))
+
+# Compares windows to all other windows, if similar add to the same bin
+for ia,a in enumerate(windows):            
+    bin_id = len(bins)
+    texture = np.array(a[1])
+    hist = a[2]
+    for b in windows[:ia]:
+        if a == b:
+            continue
         
-        bin_id = len(bins)
-        for w in windows:
-            #print(texture)
-            #print(w[1])
-            text_dist = np.linalg.norm(texture - np.array(w[1]))
-            #print(texture - w[1])
-            #print("texture dist {0}".format(text_dist))
-            # hist = hist.tolist()
-            # hist = np.array(hist).flatten()
-            # print(type(hist))
-            # print(hist.shape)
-            # hist2 = np.array(w[2]).flatten()
-            hist2 = w[2]
-            # print(type(hist2))
-            # print(hist2.shape)
-            col_dist = (1 - cv2.compareHist(hist, hist2, 0))
-            #print("color dist {0}".format(col_dist))
-            # total_dist = math.sqrt(math.pow(text_dist, 2) + math.pow(col_dist, 2))
-            # col_weighted_dist = col_dist * text_dist
-            # if total_dist < thresh:
-            if col_dist < COLTHRESH and text_dist < TEXTHRESH:
-                bin_id = w[4]
-                #print(bin_id)
-                #print("total dist {0}".format(total_dist))
-                # cv2.imshow('comp1', window)
-                # cv2.imshow('comp2', w[3])
-                # cv2.waitKey(0)
-                # cv2.destroyAllWindows()
-                break
-
-
-
-        windows.append([block_id, texture, hist, window, bin_id, [l, t, r, b]])
-        if bin_id == len(bins):
-            bins.append([])
-        bins[bin_id].append([block_id, texture, hist, window, bin_id, [l, t, r, b]])
+        text_dist = np.linalg.norm(texture - np.array(b[1]))
+        hist2 = b[2]
+        col_dist = (1 - cv2.compareHist(hist, hist2, 0))
+        if col_dist < COLTHRESH and text_dist < TEXTHRESH:
+            bin_id = b[4]
+            break
+    if bin_id == len(bins):
+        bins.append([])
+    bins[bin_id].append([block_id, texture, hist, window, bin_id, a[5]])
+    a[4] = bin_id
         
-print(img_bigger[5:10, 5:10])
 
-
+# Draw bins onto image in different hues
 img_rect_pre_merge = img_bigger.copy()
 num_bins = len(bins)
-print(num_bins)
 hue_step = max(math.floor(180/num_bins), 1)
-print(hue_step)
 hues = list(range(0, 180, hue_step))
-print(hues)
 for bin_set in bins:
     for wind in bin_set:
         l,t,r,b = wind[5]
@@ -176,6 +168,9 @@ if DETAIL:
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+# Calculate if we can merge bins
+# We merge if any items in two bins are close enough
+# This thresh is smaller to avoid chaining
 bins_to_merge = {}
 for w in windows:
     print("window {0} / {1}".format(w[0], len(windows)))
@@ -203,7 +198,7 @@ for w in windows:
 visited = []
 new_bins = []
 
-
+# Actually merge bins with DFS
 for b in bins_to_merge.keys():
     print('merging {0}'.format(b))
     if b in visited:
@@ -227,14 +222,12 @@ for b in bins_to_merge.keys():
         
         dfs_stack = dfs_stack + list(bins_to_merge[visiting])
 
+# Draw post merge bins
 bins = new_bins
 img_rect_post_merge = img_bigger.copy()
 num_bins = len(bins)
-print(num_bins)
 hue_step = max(math.floor(180/num_bins), 1)
-print(hue_step)
 hues = list(range(0, 180, hue_step))
-print(hues)
 for bin_set in bins:
     for wind in bin_set:
         l,t,r,b = wind[5]
@@ -247,8 +240,9 @@ if DETAIL:
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+# We select the largest bin as the background
+#Here we draw these windows
 img_rect = img_bigger.copy()
-print(img_bigger[5:10, 5:10])
 bins.sort(key=len, reverse=True)
 for curr_bin in bins[0]:
     l,t,r,b = curr_bin[5]
@@ -256,41 +250,31 @@ for curr_bin in bins[0]:
     b = curr_bin[3][..., 1]
     g = curr_bin[3][..., 1]
     r = curr_bin[3][..., 2]
-    print('window')
-    print(b)
-    print(g)
-    print(r)
-# # for b in bins:
-# #     print(len(b))
-# #     cv2.imshow('comp2', b[0][3])
-# #     cv2.waitKey(0)
-# #     cv2.destroyAllWindows()  
-# print(img_bigger[5:10, 5:10])
+
 if DETAIL:
     cv2.imshow('image',cv2.cvtColor(img_orig, cv2.COLOR_HSV2BGR))
     cv2.imshow('image background segments',cv2.cvtColor(img_rect, cv2.COLOR_HSV2BGR))
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-# cv2.imwrite('save-test.png',cv2.cvtColor(img_rect, cv2.COLOR_HSV2BGR))
 
-print(img_bigger[5:10, 5:10])
+
+
+# With these windows, we can calculate the two parameters for floodfill
+# We get starting points as the center of each window
+# We get upper/lower thresh as a function of standard deviation in window
 floodflags = 4
 floodflags |= cv2.FLOODFILL_MASK_ONLY
 floodflags |= (255 << 8)
 mask = np.zeros((img_bigger.shape[0]+2, img_bigger.shape[1]+2),np.uint8)
-mask2 = np.zeros((img_bigger.shape[0]+2, img_bigger.shape[1]+2),np.uint8)
+#mask2 = np.zeros((img_bigger.shape[0]+2, img_bigger.shape[1]+2),np.uint8)
 
-bgdModel = np.zeros((1,65),np.float64)
-fgdModel = np.zeros((1,65),np.float64)
-
+# Calculate channel standard deviation over all windows
 ov_c1_std = 0
 ov_c2_std = 0
 ov_c3_std = 0
 av_c1_std = 0
 av_c2_std = 0
 av_c3_std = 0
-
-
 for curr_bin in bins[0]:
     c1 = curr_bin[3][..., 0]
     c2 = curr_bin[3][..., 1]
@@ -303,89 +287,88 @@ for curr_bin in bins[0]:
     av_c1_std += np.std(c1)
     av_c2_std += np.std(c2)
     av_c3_std += np.std(c3)
-
 av_c1_std = av_c1_std / len(bins[0])
 av_c2_std = av_c2_std / len(bins[0])
 av_c3_std = av_c3_std / len(bins[0])
 
-for curr_bin in bins[0]:
-    l,t,r,b = curr_bin[5]
-    print(l)
-    print(t)
-    print(r)
-    print(b)
-    point = (int((l+r)/2), int((t+b)/2))
-    print(img_bigger[t, l])
-    c1 = curr_bin[3][..., 0]
-    c2 = curr_bin[3][..., 1]
-    c3 = curr_bin[3][..., 2]
-    std_multi = MULTI
-    c1_std = np.std(c1)
-    c2_std = np.std(c2)
-    c3_std = np.std(c3)
-    c1_std = (ov_c1_std + c1_std * 3) / 4
-    c2_std = (ov_c2_std + c2_std * 3) / 4
-    c3_std = (ov_c3_std + c3_std * 3) / 4
-    c1_std = (av_c1_std + c1_std * 2) / 3
-    c2_std = (av_c2_std + c2_std * 2) / 3
-    c3_std = (av_c3_std + c3_std * 2) / 3
-    # #curr_bin_hsv = cv2.cvt
-    # print('window')
-    # print(b)
-    # print(g)
-    # print(r)
-    # print(img_bigger[point[1], point[0]])
-    # cv2.imshow('img', cv2.cvtColor(curr_bin[3], cv2.COLOR_HSV2BGR))
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    cv2.floodFill(img_bigger ,mask, point, 255, (c1_std * std_multi * 0.5, c2_std * std_multi, c3_std * std_multi), (np.std(c1) * std_multi, c2_std * std_multi, c3_std * std_multi * 0.5), floodflags)     # line 27
-    if DETAIL:
-        img_show_rect = img_rect.copy()
-        cv2.rectangle(img_show_rect, (l, t), (r, b), (50, 255, 255), 1)
-        
-        mask2 = np.where((mask==0), 0, 1).astype('uint8')
-        final = img_bigger*mask2[1:-1,1:-1,np.newaxis]
-        cv2.rectangle(final, (l, t), (r, b), (50, 255, 255), 1)
-        cv2.imshow('fill', cv2.cvtColor(final, cv2.COLOR_HSV2BGR))
-        cv2.imshow('rect', cv2.cvtColor(img_show_rect, cv2.COLOR_HSV2BGR))
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-mask2 = np.where((mask==0), 0, 1).astype('uint8')
-mask_inv = np.where((mask==255), 0, 1).astype('uint8')
 
-kernel = np.ones((5,5),np.uint8)
-    
-opening = cv2.morphologyEx(mask2, cv2.MORPH_OPEN, kernel)
+for c_1_m in range(10, 11):
+    for c_2_m in range(6, 7):  
+        for c_3_m in range(8, 9):
+            mask = np.zeros((img_bigger.shape[0]+2, img_bigger.shape[1]+2),np.uint8)
+            for curr_bin in bins[0]:
+                l,t,r,b = curr_bin[5]
+                point = (int((l+r)/2), int((t+b)/2))
+                c1 = curr_bin[3][..., 0]
+                c2 = curr_bin[3][..., 1]
+                c3 = curr_bin[3][..., 2]
+                std_multi = MULTI
+                c1_std = np.std(c1)
+                c2_std = np.std(c2)
+                c3_std = np.std(c3)
 
-print(mask2.shape)
-print(img_bigger.shape)
+                # We weight this windows standard deviation with total
+                c1_std = (av_c1_std + c1_std * 2) / 3
+                c2_std = (av_c2_std + c2_std * 2) / 3
+                c3_std = (av_c3_std + c3_std * 2) / 3
 
-final = img_bigger*mask2[1:-1,1:-1,np.newaxis]
-final_comp = img_bigger*mask_inv[1:-1,1:-1,np.newaxis]
-cv2.line(img_orig,(point[0] - 5,point[1]),(point[0] + 5,point[1]),(255,0,0),1)
-cv2.line(img_orig,(point[0],point[1] - 5),(point[0],point[1] + 5),(255,0,0),1)
+                # Each channel gets an empirically calced multiplyer
+                c1_thresh = MULTI * c_1_m / 10
+                c2_thresh = MULTI * c_2_m / 10
+                c3_thresh = MULTI * c_3_m / 10
 
-background = img_bigger - final_comp
+                # Floodfill sets mask to 255 where background is
+                cv2.floodFill(img_bigger ,mask, point, 255, (c1_std * c1_thresh, c2_std * c2_thresh, c3_std * c3_thresh), (np.std(c1) * c1_thresh, c2_std * c2_thresh, c3_std * c3_thresh), floodflags)     # line 27
+                if DETAIL:
+                    img_show_rect = img_rect.copy()
+                    cv2.rectangle(img_show_rect, (l, t), (r, b), (50, 255, 255), 1)
+                    
+                    mask2 = np.where((mask==0), 0, 1).astype('uint8')
+                    final = img_bigger*mask2[1:-1,1:-1,np.newaxis]
+                    cv2.rectangle(final, (l, t), (r, b), (50, 255, 255), 1)
+                    cv2.imshow('fill', cv2.cvtColor(final, cv2.COLOR_HSV2BGR))
+                    cv2.imshow('rect', cv2.cvtColor(img_show_rect, cv2.COLOR_HSV2BGR))
+                    cv2.waitKey(0)
+                    cv2.destroyAllWindows()
+            
+            # Mask is 0 for foreground, 1 in background
+            mask2 = np.where((mask==0), 0, 1).astype('uint8')
+            mask_inv = np.where((mask==255), 0, 1).astype('uint8')
 
-#Change all pixels in the background that are not black to white
-background[np.where((background > [0,0,0]).all(axis = 2))] =[255,0,255]
-final_comp = background + final_comp
+            # Open to remove noise
+            kernel = np.ones((5,5),np.uint8)
+            kernel3 = np.ones((3,3),np.uint8)
+                
+            opening = cv2.morphologyEx(mask2, cv2.MORPH_OPEN, kernel)
+            opening = cv2.dilate(opening,kernel3,iterations = 1)
 
+            print(mask2.shape)
+            print(img_bigger.shape)
+
+            # Final comp is the components
+            final = img_bigger*opening[1:-1,1:-1,np.newaxis]
+            final_comp = img_bigger*mask_inv[1:-1,1:-1,np.newaxis]
+            background = img_bigger - final_comp
+
+            #Change all pixels in the background that are not black to white
+            background[np.where((background > [0,0,0]).all(axis = 2))] =[255,0,255]
+            final_comp = background + final_comp
+            cv2.imwrite('{0}//test3-{1}-{2}-{3}.png'.format(o_dir, c_1_m, c_2_m, c_3_m),cv2.cvtColor(final_comp, cv2.COLOR_HSV2BGR))
+
+# Get contours of the mask
 im2, contours, hierarchy = cv2.findContours(mask2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+# Simplify these and cull small objects
 approxes = []
 for c in contours:
-    # epsilon = 0.005*cv2.arcLength(c,True)
     if cv2.arcLength(c,True) > 50:
         epsilon = 2
         approx = cv2.approxPolyDP(c,epsilon,True)
         approxes.append(approx)
 
-img_contour = img_bigger.copy()
-img_contour2 = img_bigger.copy()
-img_contour_indi = img_bigger.copy()
-cv2.drawContours(img_contour, contours, -1, (0,0,255), 3)
-cv2.drawContours(img_contour2, approxes, -1, (0,0,255), 3)
 
+# Crop components to seperate images
+img_contour_indi = img_bigger.copy()
 
 re_x = 250
 re_y = 250
@@ -429,24 +412,10 @@ for i,a in enumerate(approxes):
 
 
 
-# cv2.imshow('components',cv2.cvtColor(final_comp, cv2.COLOR_HSV2BGR))
-# cv2.imshow('components_a',cv2.cvtColor(approxed_comp, cv2.COLOR_HSV2BGR))
-# cv2.imshow('img', cv2.cvtColor(img_bigger, cv2.COLOR_HSV2BGR))
-# cv2.imshow('image-rects',cv2.cvtColor(img_rect, cv2.COLOR_HSV2BGR))
-# cv2.imshow('background', cv2.cvtColor(final, cv2.COLOR_HSV2BGR))
-# cv2.imshow('finala', cv2.cvtColor(final_a, cv2.COLOR_HSV2BGR))
-# cv2.imshow('con', cv2.cvtColor(img_contour, cv2.COLOR_HSV2BGR))
-# cv2.imshow('con-approx', cv2.cvtColor(img_contour2, cv2.COLOR_HSV2BGR))
-# cv2.imshow('mask', mask2)
-# cv2.imshow('im2', im2)
+
 cv2.imwrite('{4}seg-components-{0}-{1}-{2}-{3}.png'.format(FILE, BLOCK_SIZE, COLTHRESH, TEXTHRESH, o_dir),cv2.cvtColor(final_comp, cv2.COLOR_HSV2BGR))
 cv2.imwrite('{4}seg-image-rects-{0}-{1}-{2}-{3}.png'.format(FILE, BLOCK_SIZE, COLTHRESH, TEXTHRESH, o_dir),cv2.cvtColor(img_rect, cv2.COLOR_HSV2BGR))
 cv2.imwrite('{4}seg-background-{0}-{1}-{2}-{3}.png'.format(FILE, BLOCK_SIZE, COLTHRESH, TEXTHRESH, o_dir), cv2.cvtColor(final, cv2.COLOR_HSV2BGR))
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-# plt.imshow(approxed_mask)
-# plt.colorbar()
-# plt.show()
 
 with open('{0}run.txt'.format(o_dir), 'w+') as f:
     f.write("python seg-auto.py --file {0} --block {1} --texthresh {2} --colthresh {3} --multi {4}".format(FILE, BLOCK_SIZE, TEXTHRESH, COLTHRESH, MULTI))
